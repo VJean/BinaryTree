f0cfd028fa730b4beb8b1591f1f6a58e3d8c9c7d
Impellettieri Florian <florian.impellettieri@etu.utc.fr>
2016-04-06 15:58:21 +0200

    ajout des 3 premiers algorithmes de l'AnalyseAgent

--- src/sudoku/AnalyseAgent.java
+++ src/sudoku/AnalyseAgent.java
@@ -1,5 +1,9 @@
 package sudoku;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
@@ -9,6 +13,7 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+import jade.util.leap.HashMap;
 
 
 public class AnalyseAgent extends Agent {
@@ -81,7 +86,68 @@ public class AnalyseAgent extends Agent {
         }
     }
 
-    private CaseGrille[] HandleCases(CaseGrille[] set){
-        return set;
+    private void handleCases(CaseGrille[] set){
+    	Map<Integer, ArrayList<Integer>> possibleToIndex = (Map<Integer, ArrayList<Integer>>) new HashMap();
+    	for(int i=0;i<set.length;i++){
+    		if(set[i].getValeur() == 0){
+    			uniqPossibleValue(set[i]);
+    			// On met dans la map les index qui correspondent à chaque valeur des possibles
+    			for(int j=0;j<set[i].getPossibles().size();j++){
+    				int valuePossible = set[i].getPossibles().get(j);
+    				// Si la valeur du possible existe déjà dans la map, on ajoute l'index correspondant
+    				if(possibleToIndex.containsKey(valuePossible)){
+    					possibleToIndex.get(valuePossible).add(i);
+    				}
+    				// Sinon, on ajoute une entrée dans la map avec le premier index à ajouter
+    				else{
+    					possibleToIndex.put(valuePossible, new ArrayList<Integer>(i));
+    				}
+    			}
+    		}
+    		else{
+    			removePossibleValue(set, i);
+    		}
+    	}
+    	uniqIndexToPossibleValue(set, possibleToIndex);
     }
+
+    /**
+     * Si une valeur n'est possible que pour une case, alors celle-ci prend cette valeur
+     * @param possibleToIndex : valeurs possibles mappées à l'ensemble des index correspondants
+     */
+    private void uniqIndexToPossibleValue(CaseGrille[] set, Map<Integer, ArrayList<Integer>> possibleToIndex) {
+    	for (Entry<Integer, ArrayList<Integer>> entry : possibleToIndex.entrySet()) {
+			Integer value = entry.getKey();
+			ArrayList<Integer> indexes = entry.getValue();
+			
+    		if(indexes.size() == 1){
+    			set[indexes.get(0)].setValeur(value);
+    		}
+		}
+	}
+
+	/**
+     * Supprime la valeur d'une case à la liste des possibles des autres cases non déterminées de son tableau
+     * @param set : tableau des cases à modifier
+     * @param i : index de la case à ne pas modifier (dont on utilise la valeur)
+     */
+    private void removePossibleValue(CaseGrille[] set, int i) {
+    	int valueToRemove =  set[i].getValeur();
+		for(int j=0;j<set.length;j++){
+			if(j!=i && set[j].getValeur()==0){
+				set[j].getPossibles().remove((Integer)valueToRemove);
+			}
+		}
+	}
+
+    /**
+     * Donne à la valeur de la case l'unique valeur possible, si c'est le cas
+     * @param caseG : case à modifier
+     */
+	private void uniqPossibleValue(CaseGrille caseG) {
+		if(caseG.getPossibles().size() == 1){
+			caseG.setValeur(caseG.getPossibles().get(0));	
+			caseG.getPossibles().remove(0);
+		}
+	}
 }
