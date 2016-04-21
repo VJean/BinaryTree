11e7c5c5b26d9397a71324f550161dd74d5f0050
Vintache Jean <jean.vintache@etu.utc.fr>
2016-04-21 18:46:25 +0200

    missing td5 prefix in ABox ; parse ABox in main()

--- src/ontology/TD5Application.java
+++ src/ontology/TD5Application.java
@@ -1,11 +1,19 @@
 package ontology;
 
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
+
+import java.io.InputStream;
+
 /**
  * Created by JeanV on 21/04/2016.
  */
 public class TD5Application {
     public static void main(String[] args) {
+        Model model = ModelFactory.createDefaultModel();
+        InputStream in = FileManager.get().open("file:res/td5.n3");
+        model.read(in, null, "TURTLE");
 
     }
-
 }
