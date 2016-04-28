14945a53bc4060e75c9fe2e9ebfb8bad0cbad120
Vintache Jean <jean.vintache@etu.utc.fr>
2016-04-28 18:25:25 +0200

    KB agent registers to DF

--- src/ontology/KBAgent.java
+++ src/ontology/KBAgent.java
@@ -2,12 +2,29 @@ package ontology;
 
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 public class KBAgent extends Agent{
 	protected void setup() {
 		System.out.println("'" + getLocalName() + "' initiated.\t(" + this.getClass() + ")" );
+
+		// register to Directory Facilitator (DF)
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("KnowledgeBase");
+		sd.setName(getLocalName());
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
 
         // add request Behaviour
         addBehaviour(new RequestBehaviour());
