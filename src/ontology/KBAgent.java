39300e8c6d7a5c52d47205564756165db0615e39
Impellettieri Florian <florian.impellettieri@etu.utc.fr>
2016-04-28 19:02:35 +0200

    complétion du KBAgent version TD6 git add src/*git add src/*git add src/*git add src/*git add src/*git add src/*

--- src/ontology/KBAgent.java
+++ src/ontology/KBAgent.java
@@ -1,17 +1,40 @@
 package ontology;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
+
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 public class KBAgent extends Agent{
+	
+	private Model model;
+	
 	protected void setup() {
 		System.out.println("'" + getLocalName() + "' initiated.\t(" + this.getClass() + ")" );
-
+		
+	    Model model = ModelFactory.createDefaultModel();
+		InputStream in = FileManager.get().open("file:res/td5.n3");
+        model.read(in, null, "TURTLE");
+        
         // add request Behaviour
         addBehaviour(new RequestBehaviour());
     }
+	public Model getModel(){
+		return model;
+	}
 	
 	private class RequestBehaviour extends Behaviour{
 
@@ -19,8 +42,22 @@ public class KBAgent extends Agent{
 		public void action() {
 			MessageTemplate msgTemplate = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
             ACLMessage msg = receive(msgTemplate);
-            
-            
+    		
+            if (msg != null) {
+            	String query = msg.getContent();
+            	String result = runSelectQuery(query, getModel());
+            	ACLMessage msgReply = msg.createReply();
+            	if (result==null){
+            		msgReply.setPerformative(ACLMessage.FAILURE);
+            		msgReply.setContent("Error while executing the query");
+            		send(msgReply);
+            	}
+            	else{
+            		msgReply.setPerformative(ACLMessage.INFORM);
+            		msgReply.setContent(result);
+            		send(msgReply);
+            	}
+            }
 			
 		}
 
@@ -31,5 +68,15 @@ public class KBAgent extends Agent{
 		}
 		
 	}
+	public static String runSelectQuery(String query, Model model) {
+
+        QueryExecution queryExecution = QueryExecutionFactory.create(query, model);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ResultSet r = queryExecution.execSelect();
+        ResultSetFormatter.outputAsCSV(baos,r);
+        queryExecution.close();
+        
+        return baos.toString();
+    }
 
 }
