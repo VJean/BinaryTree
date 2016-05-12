68189081489969fe57a6d17810f31a4e798c414a
Vintache Jean <jean.vintache@etu.utc.fr>
2016-05-12 17:19:56 +0200

    Add constants class

---
+++ src/simulation/model/Constants.java
@@ -0,0 +1,17 @@
+package simulation.model;
+
+/**
+ * Created by JeanV on 12/05/2016.
+ */
+public class Constants {
+    static public int GRID_SIZE = 30;
+    static public int NUM_INSECT = 10;
+    static public int NUM_FOOD_CELL = 15;
+    static public int MAX_ENERGY = 15;
+    static public int MAX_LOAD = 5;
+    static public int CAPACITY_POINTS = 10;
+    static public int MAX_FOOD = 5;
+    static public int FOOD_ENERGY = 3;
+
+    private Constants() {}
+}
